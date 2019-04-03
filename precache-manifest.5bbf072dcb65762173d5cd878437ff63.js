073f3c1de794c5a5b0f23f0d43ac3d1bc6f0aadc
Danica <danica.pivalica@gmail.com>
2019-04-03 23:13:09 +0200

    initial commit

---
+++ precache-manifest.5bbf072dcb65762173d5cd878437ff63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a655fef9a236a933dbc",
+    "url": "/static/js/main.34a66e62.chunk.js"
+  },
+  {
+    "revision": "b32982f4fdd15b22ba79",
+    "url": "/static/js/2.87af39e9.chunk.js"
+  },
+  {
+    "revision": "7a655fef9a236a933dbc",
+    "url": "/static/css/main.3cef09fa.chunk.css"
+  },
+  {
+    "revision": "05399ae0869af5da3437e4494ef7a9c5",
+    "url": "/index.html"
+  }
+];
